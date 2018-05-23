b348a3f3d76fc2f522f83a2e9eaff305e852d50e
AllanRobert0 <37585575+AllanRobert0@users.noreply.github.com>
2018-05-23 16:44:37 -0300

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,12 @@
+public class ex1{
+	public static void main(String args[]){
+		
+		double C;
+		
+		C = Input.readDouble("	Digite temp. Celcius: ");
+		
+	
+		System.out.println("Temperatura em F : "+(9*C+160)/5);
+	
+	}
+}
